fd77f65421a7ea0b896a5c1a0d957bf7cbe1acf7
KrGuddu <krguddu684@gmail.com>
2026-01-04 11:54:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./assets/css/style.css";
+// import "./assets/css/style"
 
-function App() {
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Products from './components/Products';
+import Review from './components/Review';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Footer from './components/Footer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Menu/>
+    <Products/>
+    <Review/>
+    <Contact/>
+    <Blog/>
+    <Footer/>
+    </>
   );
 }
 
